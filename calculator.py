0fc19f7fca1f3293574ace95b14160a0ce75ccc2
JaneTheExplorer <janemugo2004@gmail.com>
2025-07-29 21:41:13 +0300

    Added calculator.py

---
+++ calculator.py
@@ -0,0 +1,6 @@
+num1 = float(input("Enter first number:"))
+num2 = float(input("Enter secnd number:"))
+operation = input("Enter operation (*):")
+if operation == "*":
+    result = num1 * num2
+print(f"{num1} * {num2} ={result}")
